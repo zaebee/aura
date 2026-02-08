1ad4d3b3a1baaabbd1b547f1bea6141efbd31184
zaebee <zaebuntu@gmail.com>
2026-02-09 02:46:22 +0300

    chore: fixed linter

--- synapses/telegram-bot/src/nats_adapter.py
+++ synapses/telegram-bot/src/nats_adapter.py
@@ -9,7 +9,8 @@ Two channels:
 import nats
 import nats.errors
 import structlog
-from aura_core.gen.aura.dna.v1 import Observation as ProtoObservation, Signal
+from aura_core.gen.aura.dna.v1 import Observation as ProtoObservation
+from aura_core.gen.aura.dna.v1 import Signal
 
 logger = structlog.get_logger(__name__)
 
